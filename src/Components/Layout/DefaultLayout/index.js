170afadbdc7ecadc9fc1e5381c84b3d20bda18da
VuVanQuyen1999 <VuVanQuyen07081999@gmail.com>
2022-06-02 14:59:27 +0700

    Dựng khung layout

--- src/Components/Layout/DefaultLayout/index.js
+++ src/Components/Layout/DefaultLayout/index.js
@@ -1,13 +1,18 @@
+import classNames from 'classnames/bind';
+
 import Header from '~/Components/Layout/components/Header';
 import Sidebar from './Sidebar';
+import styles from './Default.module.scss';
+
+const cx = classNames.bind(styles);
 
 function DefaultLayout({ children }) {
     return (
-        <div>
+        <div className={cx('wrapper')}>
             <Header />
-            <div className="container">
+            <div className={cx('container')}>
                 <Sidebar />
-                <div className="content">{children}</div>
+                <div className={cx('content')}>{children}</div>
             </div>
         </div>
     );
