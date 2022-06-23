d063d5981ec45c71e127f4480f24923f988776ef
VuVanQuyen1999 <VuVanQuyen07081999@gmail.com>
2022-06-23 09:40:14 +0700

    commit 23/06/2022

--- src/Components/Layout/components/Header/index.js
+++ src/Components/Layout/components/Header/index.js
@@ -26,6 +26,8 @@ import images from '~/assets/img';
 import { Wrapper as PopperWrapper } from '~/Components/Popper';
 import AccountItem from '~/Components/AccountItem';
 import Menu from '~/Components/Popper/Menu';
+import { MailboxIcon, MessageIcon } from '~/Components/Icons';
+import Image from '~/Components/images';
 
 const cx = classNames.bind(styles);
 const MENU_ITEMS = [
@@ -149,6 +151,17 @@ function Header() {
                                         <FontAwesomeIcon icon={faCloudUpload} />
                                     </button>
                                 </Tippy>
+                                <Tippy delay={[0, 200]} content="Tin nhắn" placement="bottom">
+                                    <button className={cx('actions-btn')}>
+                                        <MessageIcon />
+                                    </button>
+                                </Tippy>
+                                <Tippy delay={[0, 200]} content="Hộp thư" placement="bottom">
+                                    <button className={cx('actions-btn')}>
+                                        <MailboxIcon />
+                                        <span>18</span>
+                                    </button>
+                                </Tippy>
                             </div>
                         </>
                     ) : (
@@ -160,10 +173,11 @@ function Header() {
 
                     <Menu items={currentUser ? userMenu : MENU_ITEMS} onChange={handleMenuChange}>
                         {currentUser ? (
-                            <img
+                            <Image
                                 className={cx('user-avatar')}
                                 src="https://p16-sign-va.tiktokcdn.com/tos-useast2a-avt-0068-aiso/e7b9db475cba9fa98f1b7e055bf6712d~c5_100x100.jpeg?x-expires=1655953200&x-signature=frQ6AXiNTLCzdW881B7tq9TIIlk%3D"
                                 alt="Nguyen Van A"
+                                // fallBack="https://www.google.com/imgres?imgurl=https%3A%2F%2Fi.pinimg.com%2F550x%2F56%2F1a%2F28%2F561a288a3e202eb2cd08e2d300f236a3.jpg&imgrefurl=https%3A%2F%2Fwww.pinterest.com%2Fthaongoc258%2Fr%25C3%25A1i-c%25C3%25A1-w%2F&tbnid=817oetChINnqXM&vet=12ahUKEwj6rIfxxML4AhV90IsBHY2DAdkQMygregUIARCSAg..i&docid=LzAT4KMdLM-yAM&w=550&h=825&q=r%C3%A1i%20c%C3%A1&ved=2ahUKEwj6rIfxxML4AhV90IsBHY2DAdkQMygregUIARCSAg"
                             />
                         ) : (
                             <button className={cx('more-btn')}>
